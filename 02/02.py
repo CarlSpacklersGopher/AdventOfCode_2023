040ff36eebf50f082db92dff423e4a6c7b546ee3
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-02 09:13:21 -0600

    Added Day 2 prompt.  Renamed input files.  Solved for Day 2.

--- 02/02.py
+++ 02/02.py
@@ -1,9 +1,11 @@
 import re
+from math import prod
 
 MAX_CUBES = {'red': 12, 'green': 13, 'blue': 14}
 
 
 class Game:
+    ''' Represents a single game with multiple draws of different colored cubes'''
     # draws: list[dict[color,count]]
     def __init__(self, game:int, draws:list[dict]):
         self.game_number = game
@@ -16,6 +18,18 @@ class Game:
                 if count > MAX_CUBES[color]:
                     return False
         return True
+
+    def min_cubes(self) -> dict[str, int]:
+        ''' Finds the minimum amount of cubes of each color to make the game possible.'''
+        minimums = {}
+        for draw in self.draws:
+            for color, count in draw.items():
+                if color not in minimums:
+                    minimums[color] = count
+                else:
+                    if count > minimums[color]:
+                        minimums[color] = count
+        return minimums
 
 
 
@@ -49,15 +63,20 @@ def part_1(games: list[Game]) -> int:
 
 
 
-def part_2(input_lines: list) -> int:
+def part_2(games: list[Game]) -> int:
     ''' Solves for part 2.  '''
-    return 0
+    # Probably could solve this by calling is_game_possible to eliminate some of the checks.
+    # But this is simpler :)
+    game_powers = []
+    for game in games:
+        min_cubes = game.min_cubes()
+        game_powers.append(prod(min_cubes.values()))
+    return sum(game_powers)
 
 if __name__ == "__main__":
-    # puzzle_input = process_input('02/test_input1.txt')
+    # puzzle_input = process_input('02/test_input.txt')
     puzzle_input = process_input('02/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
 
-    # puzzle_input = process_input('02/test_input2.txt')
     print("Part 2: " + str(part_2(puzzle_input)))
 
