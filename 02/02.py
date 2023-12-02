9e80e318f1c067b9495d9e6944759de8b1e8b23f
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-02 01:16:25 -0600

    day 2 prompt

---
+++ 02/02.py
@@ -0,0 +1,23 @@
+
+def process_input(path: str) -> list:
+    ''' Reads the puzzle input.'''
+    with open(path, 'r') as f:
+        return f.readlines()
+
+def part_1(input_lines: list) -> int:
+    ''' Solves for part 1. '''
+    return 0
+
+
+def part_2(input_lines: list) -> int:
+    ''' Solves for part 2.  '''
+    return 0
+
+if __name__ == "__main__":
+    # puzzle_input = process_input('01/test_input1.txt')
+    puzzle_input = process_input('01/input.txt')
+    print("Part 1: " + str(part_1(puzzle_input)))
+
+    # puzzle_input = process_input('01/test_input2.txt')
+    print("Part 2: " + str(part_2(puzzle_input)))
+
