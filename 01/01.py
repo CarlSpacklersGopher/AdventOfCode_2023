0bd324d383852344fd3084b225713576f98a852c
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-01 23:09:40 -0600

    added part 2 prompt

--- 01/01.py
+++ 01/01.py
@@ -13,9 +13,14 @@ def part_1(puzzle_input: list) -> int:
         calibrations.append(int(numbers[0] + numbers[-1]))
     return sum(calibrations)
 
+def part_2(puzzle_input: list) -> int:
+    ''' Solves for part 2.  '''
+    pass
+
 if __name__ == "__main__":
-    # puzzle_input = process_input('01/test_input.txt')
-    puzzle_input = process_input('01/input.txt')
+    # puzzle_input = process_input('01/test_input1.txt')
+    puzzle_input = process_input('01/test_input2.txt')
+    # puzzle_input = process_input('01/input.txt')
     pt1 = part_1(puzzle_input)
     print("Part 1: " + str(pt1))
 
