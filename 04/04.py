9c7c0549d1984d215447be58aea78ec10a1bebde
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-04 23:12:49 -0600

    part 1 solved

--- 04/04.py
+++ 04/04.py
@@ -1,12 +1,28 @@
-def process_input(path: str) -> list:
+import pprint
+
+def process_input(path: str) -> zip:
     ''' Reads the puzzle input.'''
     with open(path, 'r') as f:
-        return f.readlines()
+        winners = []
+        card_numbers = []
+        for line in f:
+            winner_chunk, my_chunk = line.split('|')
+            winners.append(winner_chunk.split()[2:]) # Split off "Card x:"
+            card_numbers.append(my_chunk.split())
+    return zip(winners, card_numbers)
 
-
-def part_1(lines: list[str]) -> int:
+def part_1(cards: zip) -> int:
     ''' Solves for part 1. '''
-    return 0
+    card_points = []
+    for winners, card_numbers in cards:
+        win_count = 0
+        for winning_number in winners:
+            for my_number in card_numbers:
+                if int(winning_number) == int(my_number):
+                    win_count += 1
+        card_points.append(0 if win_count == 0 else 2 ** (win_count - 1))
+
+    return sum(card_points)
 
 def part_2(lines: list[str]) -> int:
     ''' Solves for part 2.  '''
