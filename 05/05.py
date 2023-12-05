94d9f3a86c7552613d6a8c5d18a3b06ec608bfa0
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-05 00:34:04 -0600

    day 5 prompt and skeleton

---
+++ 05/05.py
@@ -0,0 +1,24 @@
+
+def process_input(path: str) -> list[str]:
+    ''' Reads the puzzle input.'''
+    with open(path, 'r') as f:
+        return f.readlines()
+
+def part_1(puzzle_input: list[str]) -> int:
+    ''' Solves for part 1. '''
+    return 0
+
+def part_2(puzzle_input: list[str]) -> int:
+    ''' Solves for part 2.  '''
+    return 0
+
+
+if __name__ == "__main__":
+
+    puzzle_input = process_input('05/test_input.txt')
+    # puzzle_input = process_input('05/input.txt')
+    print("Part 1: " + str(part_1(puzzle_input)))
+
+    print("Part 2: " + str(part_2(puzzle_input)))
+
+
