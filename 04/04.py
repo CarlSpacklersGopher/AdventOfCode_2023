bf91f7a761d66fb35185ce8c33c6c346f7acc43b
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-05 00:01:16 -0600

    Refactored process_input to make card numbers and card copy count available.  part 2 prompt as well.

--- 04/04.py
+++ 04/04.py
@@ -1,28 +1,31 @@
-import pprint
 
-def process_input(path: str) -> zip:
+def process_input(path: str) -> dict:
     ''' Reads the puzzle input.'''
     with open(path, 'r') as f:
-        winners = []
-        card_numbers = []
-        for line in f:
+        COPIES = 1
+        card_dict = {}
+        for idx, line in enumerate(f):
             winner_chunk, my_chunk = line.split('|')
-            winners.append(winner_chunk.split()[2:]) # Split off "Card x:"
-            card_numbers.append(my_chunk.split())
-    return zip(winners, card_numbers)
+            winners = winner_chunk.split()[2:]
+            card_numbers = my_chunk.split()
+            card_dict[idx + 1] = [COPIES, winners, card_numbers]
+    return card_dict
 
-def part_1(cards: zip) -> int:
+def part_1(cards: dict) -> int:
     ''' Solves for part 1. '''
     # Using sets is fast, but might get weird if we have duplicates in the card's numbers that win
     card_points = []
-    for winners, card_numbers in cards:
-        my_numbers = set(card_numbers)
-        win_count = len(my_numbers.intersection(winners))
-        card_points.append(0 if win_count == 0 else 2 ** (win_count - 1))
+    for _, winners, card_numbers in cards.values():
+        wins = count_wins(winners, card_numbers)
+        card_points.append(0 if wins == 0 else 2 ** (wins - 1))
 
     return sum(card_points)
 
-def part_2(lines: list[str]) -> int:
+def count_wins(winning_numbers: list[int], my_numbers: list[int]) -> int:
+        return len(set(my_numbers).intersection(winning_numbers))
+
+
+def part_2(cards: dict) -> int:
     ''' Solves for part 2.  '''
     return 0
 
