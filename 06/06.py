5e15b1a42f8cafb98ca9f449e4d82fcc15a7fc88
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-06 01:26:07 -0600

    solved part 2.  This is much more of a solution as a math problem vs brute force in part 1.

--- 06/06.py
+++ 06/06.py
@@ -1,12 +1,20 @@
-from math import prod
+import math
 
-def process_input(path: str) -> zip:
+def process_split_input(path: str) -> zip:
     ''' Reads the puzzle input.'''
     with open(path, 'r') as f:
         lines = f.readlines()
         times = [int(x) for x in lines[0].split()[1:]]
         distances = [int(x) for x in lines[1].split()[1:]]
         return zip(times, distances)
+
+def process_combined_input(path: str) -> tuple[int]:
+    ''' Reads the puzzle input. '''
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        time = int(''.join(lines[0].split()[1:]))
+        distance = int(''.join(lines[1].split()[1:]))
+        return (time, distance)
 
 def part_1(race_records: zip) -> int:
     ''' Solves for part 1. '''
@@ -20,19 +28,33 @@ def part_1(race_records: zip) -> int:
             if my_distance > record_distance:
                 win_count += 1
         winning_combinations.append(win_count)
-    print(winning_combinations)
-    return prod(winning_combinations)
+    return math.prod(winning_combinations)
 
-def part_2(puzzle_input: list[str]) -> int:
+def part_2(race_record: tuple[int]) -> int:
     ''' Solves for part 2.  '''
-    return 0
+    # dist = charge_time * (time - charge_time)
+    #   0  = -charge_time**2 + time*charge_time - dist
+    #   0  = charge_time**2 - time*charge_time + dist
+    # find max dist by taking derivative wrt charge_time
+    # dist' = 2*charge_time - time
+    # charge_time = time/2 to yield max distance
+    time, distance = race_record
+    optimal_charge_time = time / 2
+    # charge_time to get current record - solve with quadratic formula
+    record_charge_time = (time + math.sqrt((-1*time)**2 - 4 * (1) * distance)) / (2 * 1)
+
+    # symmetrical on both sides
+    winning_time_count = round(2 * abs(record_charge_time - optimal_charge_time))
+    return winning_time_count
 
 
 if __name__ == "__main__":
-    # puzzle_input = process_input('06/test_input.txt')
-    puzzle_input = process_input('06/input.txt')
+    # puzzle_input = process_split_input('06/test_input.txt')
+    puzzle_input = process_split_input('06/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
 
+    # puzzle_input = process_combined_input('06/test_input.txt')
+    puzzle_input = process_combined_input('06/input.txt')
     print("Part 2: " + str(part_2(puzzle_input)))
 
 
