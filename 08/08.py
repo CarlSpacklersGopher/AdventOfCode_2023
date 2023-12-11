e9514d3517a0b8423e766028cb2a0fd0c45ddd80
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-11 00:01:55 -0600

    part 2 solved

--- 08/08.py
+++ 08/08.py
@@ -1,4 +1,4 @@
-
+import math
 
 def process_input(path: str) -> list[str]:
     ''' Reads the puzzle input. '''
@@ -29,18 +29,44 @@ def part_1(puzzle_input:list[str]) -> int:
         for direction in directions:
             if current_location == end_location:
                 return len(visited)
-            lookup = 0 if direction == 'L' else 1
-            current_location = location_dict[current_location][lookup]
+            current_location = get_next_location(direction, location_dict, current_location)
             visited.append(current_location)
 
+def get_next_location(direction:str, location_map:dict, current_location:str) -> str:
+    lookup = 0 if direction == 'L' else 1
+    next_location = location_map[current_location][lookup]
+    return next_location
 
 def part_2(puzzle_input:list[str]) -> int:
     ''' Solves for Part 2. '''
-    return 0
+    directions = [direction for direction in puzzle_input[0].strip()]
+    location_dict = get_locations(puzzle_input[2:])
+
+    current_locations = []
+    for key in location_dict:
+        if key.endswith('A'):
+            current_locations.append(key)
+
+    visit_counts = []
+    for location in current_locations:
+        visited = []
+        break_out = False
+        while not break_out:
+            for direction in directions:
+                if location.endswith('Z'):
+                    visit_counts.append(len(visited))
+                    break_out = True
+                    break
+                location = get_next_location(direction, location_dict, location)
+                visited.append(location)
+
+    return math.lcm(*visit_counts)
+
 
 if __name__ == '__main__':
     # puzzle_input = process_input('08/test_input.txt')
     puzzle_input = process_input('08/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
 
+    # puzzle_input = process_input('08/test_input3.txt')
     print("Part 2: " + str(part_2(puzzle_input)))
