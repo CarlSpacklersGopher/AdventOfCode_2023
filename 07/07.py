08345ba8397af273eee19b22fbd9ef9c86ae6fe4
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-10 19:35:49 -0600

    part 2 with jokers wild solved.

--- 07/07.py
+++ 07/07.py
@@ -10,7 +10,8 @@ card_values = {'A' : 14,
                '5' : 5,
                '4' : 4,
                '3' : 3,
-               '2' : 2
+               '2' : 2,
+               '*' : 1
                }
 
 def process_input(path: str) -> list[str]:
@@ -18,8 +19,7 @@ def process_input(path: str) -> list[str]:
     with open(path, 'r') as f:
         return f.readlines()
 
-
-def get_hand_score(hand:str) -> int:
+def get_hand_score(hand:str, jokers_wild:bool=False) -> int:
     '''Returns 27 bit number containing score of a 5 card hand.  Bit def:
     Bit 26 (MSB) - 5 of a kind
     Bit 25 - 4 of a kind
@@ -36,31 +36,47 @@ def get_hand_score(hand:str) -> int:
     Bit 4-7 - score of 4th card
     Bit 0(LSB)-3 - score of 5th card
 
-    Tiebreaker tacked onto end allows quick comparison per rules
+    Tiebreaker tacked onto end allows quick comparison per rules.
+
+    If jokers_wild is True, Any J in the hand can act as any other card to make the hand stronger.
+    Jokers have the lowest point value.
     '''
-    unique_count = len(set(hand))
-    first_count = hand.count(hand[0])
+
+    if jokers_wild:
+        joker_count = hand.count('*')
+        joker_supplement = min(1, joker_count) # To be added when checking for uniques.
+    else:
+        joker_count = 0
+        joker_supplement = 0
+
+    uniques = set(hand)
+    unique_count = len(uniques)
+    # This will cause problems.  Need to check non-joker and not count=1
+    non_jokers = list(uniques.difference({'*'}))
+    non_joker_count = 0
+    if non_jokers:
+        non_joker_count = hand.count(non_jokers[0])
+
     card_scores = [card_values[card] for card in hand]
 
     hand_score = 0
     # Hand Category
-    if unique_count == 1:
+    if unique_count - joker_supplement <= 1:
         # 5 of a kind
         hand_score = 1 << 26
-    elif unique_count == 2:
-        if (first_count == 4) or (first_count == 1):
+    elif unique_count - joker_supplement == 2:
+        if (non_joker_count + joker_count == 4) or (non_joker_count == 1):
             hand_score = 1 << 25 # 4 of a kind
         else:
             hand_score = 1 << 24 # Full House
-    elif unique_count == 3:
-        check_count = first_count
-        if check_count == 1: # Not useful for determining 3 of a kind vs 2 pair.
-            check_count = hand.count(hand[1]) # Throw away the single, get something new
-        if (check_count == 3) or (check_count == 1): # Second instance of 1, so know 3 of a kind
+    elif unique_count - joker_supplement == 3:
+        if non_joker_count == 1: # Not useful for determining 3 of a kind vs 2 pair.
+            non_joker_count = hand.count(non_jokers[1]) # Throw away the single, get something new
+        if (non_joker_count + joker_count == 3) or (non_joker_count == 1): # Second instance of 1, so know 3 of a kind
             hand_score = 1 << 23 # 3 of a kind
         else:
             hand_score = 1 << 22 # 2 pair
-    elif unique_count == 4:
+    elif unique_count - joker_supplement == 4:
         hand_score = 1 << 21 # 1 pair
     else:
         hand_score = 1 << 20 # High Card
@@ -92,14 +108,26 @@ def part_1(puzzle_input: list[str]) -> int:
 
 def part_2(puzzle_input: list[str]) -> int:
     ''' Solves for part 2.  '''
-    return 0
+    wagers = {}
+    hands = []
+    for line in puzzle_input:
+        hand, wager = line.split()
+        wagers[hand] = int(wager)
+        hand = hand.replace('J', '*')
+        hands.append(hand)
 
+    hands.sort(key=lambda hand : get_hand_score(hand, jokers_wild=True))
+    winnings = 0
+    for idx, hand in enumerate(hands):
+        hand = hand.replace('*', 'J')
+        # print('Strength: {}, Hand: {}, Bid: {}'.format(idx+1, hand, wagers[hand]))
+        rank = idx + 1
+        winnings += wagers[hand] * rank
+    return winnings
 
 if __name__ == "__main__":
     # puzzle_input = process_input('07/test_input.txt')
     puzzle_input = process_input('07/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
-    '''
-    # puzzle_input = process_input('07/input.txt')
+
     print("Part 2: " + str(part_2(puzzle_input)))
-    '''
