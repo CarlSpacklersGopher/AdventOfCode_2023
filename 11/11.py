2b299e7150c38eeaf6d087f2292daac1da013f02
Garrett Manship <garrett.d.manship@gmail.com>
2023-12-11 23:12:13 -0600

    part 2 solved.  It's UGLY though.

--- 11/11.py
+++ 11/11.py
@@ -42,13 +42,43 @@ def part_1(galaxies:list[list]) -> int:
             total_distance += abs(start_galaxy[1] - target_galaxy[1])
     return total_distance
 
-def part_2(puzzle_input:list[str]) -> int:
+def part_2(galaxies:list[list], expansion:int) -> int:
     ''' Solves for Part 2. '''
-    return 0
+    # Expand Universe North-South
+    galaxies.sort(key=lambda galaxy: galaxy[0]) # Sort on rows
+    last_occupied_row = 0
+    cosmic_expansion = 0
+    for galaxy in galaxies:
+        current_row = galaxy[0]
+        if current_row - last_occupied_row > 1:
+            cosmic_expansion += expansion * (current_row - last_occupied_row - 1)
+        galaxy[0] += cosmic_expansion
+        last_occupied_row = current_row
+
+    # Expand Universe East-West
+    galaxies.sort(key=lambda galaxy: galaxy[1]) # Sort on cols
+    last_occupied_col = 0
+    cosmic_expansion = 0
+    for galaxy in galaxies:
+        current_col = galaxy[1]
+        if current_col - last_occupied_col > 1:
+            cosmic_expansion += expansion * (current_col - last_occupied_col - 1)
+        galaxy[1] += cosmic_expansion
+        last_occupied_col = current_col
+
+    # Calculate Distances
+    total_distance = 0
+    for idx, start_galaxy in enumerate(galaxies):
+        for target_galaxy in galaxies[idx:]:
+            total_distance += abs(start_galaxy[0] - target_galaxy[0])
+            total_distance += abs(start_galaxy[1] - target_galaxy[1])
+    return total_distance
 
 if __name__ == '__main__':
     # puzzle_input = process_input('11/test_input.txt')
     puzzle_input = process_input('11/input.txt')
     print("Part 1: " + str(part_1(puzzle_input)))
 
-    print("Part 2: " + str(part_2(puzzle_input)))
+    # puzzle_input = process_input('11/test_input.txt')
+    puzzle_input = process_input('11/input.txt')
+    print("Part 2: " + str(part_2(puzzle_input, 1000000-1)))
